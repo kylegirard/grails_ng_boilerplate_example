40ed8bae82d82ba7f4608038f79f402e30c5dbbb
Kyle G <kyle@tekimpulse.com>
2013-11-18 11:44:14 -0500

    Added default Grails Project Files Added ng-app directory and pulled from https://github.com/ngbp/ng-boilerplate Created install script to install node modules

---
+++ install.sh
@@ -0,0 +1,10 @@
+#NODE.JS must be installed before running this script
+#TODO script for node.js check
+#Install if not installed
+
+cd ng-app
+sudo npm -g install grunt-cli karma bower
+npm install
+bower install
+
+
